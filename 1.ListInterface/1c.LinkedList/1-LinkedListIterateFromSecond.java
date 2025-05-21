b75700dce4d23e2cf78eb5b752e0b055d06b488d
kavyashreenayak29 <kavyashreenayak8861@gmail.com>
2025-05-21 14:19:17 +0530

    Create 1-LinkedListIterateFromSecond.java

---
+++ 1.ListInterface/1c.LinkedList/1-LinkedListIterateFromSecond.java
@@ -0,0 +1,13 @@
+//1.Write a Java program to iterate through all elements in a linked list starting at the specified position ( 2nd) using iterator ( hint : Iterator iteratorobj = listobj.listIterator(1))
+import java.util.*;
+
+public class LinkedListIterateFromSecond {
+    public static void main(String[] args) {
+        LinkedList<String> list = new LinkedList<>(Arrays.asList("Red", "Green", "Blue", "Yellow"));
+        Iterator<String> iterator = list.listIterator(1); // Start from 2nd (index 1)
+        System.out.println("Iterating from 2nd element:");
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
+}
